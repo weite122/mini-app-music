803339e264348a358763fcda9ef76602f13c3aeb
weite122 <weite122@foxmail.com>
2020-07-10 17:19:06 +0800

    [A]getPlaylist 数据去重

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -9,16 +9,31 @@ const rp = require('request-promise')
 
 const URL = 'http://musicapi.xiecheng.live/personalized'
 
-
+const playlistCollection = db.collection('playlist')
 // 云函数入口函数
 exports.main = async (event, context) => {
+  const list = await playlistCollection.get()
   const playlist = await rp(URL).then((res) => {
     return JSON.parse(res).result
   })
-  for (let i = 0, len = playlist.length; i < len; i++) {
-    await db.collection('playlist').add({
+
+  const newData = []
+  for (let i = 0, len1 = playlist.length; i < len1; i++) {
+    let flag = true
+    for (let j = 0, len2 = list.data.length; j < len2; j++) {
+      if (playlist[i].id === list.data[j].id) {
+        flag = false
+        break
+      }
+    }
+    if (flag) {
+      newData.push(playlist[i])
+    }
+  }
+  for (let i = 0, len = newData.length; i < len; i++) {
+    await playlistCollection.add({
       data: {
-        ...playlist[i],
+        ...newData[i],
         createTime: db.serverDate(),
       }
     }).then((res) => {
@@ -27,4 +42,5 @@ exports.main = async (event, context) => {
       console.error('插入失败')
     })
   }
+  return newData.length
 }
