ae214af0653446a8d68ecad4040869feb0f8bc23
weite122 <weite122@foxmail.com>
2020-07-10 13:19:56 +0800

    [A]用swiper 实现轮播

--- miniprogram/pages/playlist/playlist.js
+++ miniprogram/pages/playlist/playlist.js
@@ -11,7 +11,16 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    swiperImgUrls: [{
+      url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+    },
+    {
+      url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
+    },
+    {
+      url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
+    }
+  ]
   },
 
   /**
