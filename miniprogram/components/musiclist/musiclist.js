e2dcb1cdd03da415d0175ed7e4bd2afad3676230
weite122 <weite122@foxmail.com>
2020-07-13 08:28:41 +0800

    [A]点击歌曲后跳转至播放界面，增加歌曲已选中的样式

--- miniprogram/components/musiclist/musiclist.js
+++ miniprogram/components/musiclist/musiclist.js
@@ -11,13 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    playingId: -1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onSelect(event){
+      const musicid = event.currentTarget.dataset.musicid
+      this.setData({
+        playingId: musicid
+      })
+      wx.navigateTo({
+        url:`../../pages/player/player?musicid=${musicid}`
+      })
+    }
   }
 })
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,38 +1,18 @@
-// pages/musiclist/musiclist.js
+// pages/player/player.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    musiclist: [],
-    listInfo: {}
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.showLoading({
-      title: '加载中',
-    })
-    wx.cloud.callFunction({
-      name: 'music',
-      data: {
-        playlistId: options.playlistId,
-        $url: 'musiclist'
-      }
-    }).then((res)=> {
-      const pl = res.result.playlist
-      this.setData({
-        musiclist: pl.tracks,
-        listInfo:{
-          coverImgUrl: pl.coverImgUrl,
-          name: pl.name
-        }
-      })
-      wx.hideLoading()
-    })
+    console.log(options)
   },
 
   /**
