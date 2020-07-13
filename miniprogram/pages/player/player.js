cecd2e5dc8d5cf97fe9d3342effe34ba1f9c1722
weite122 <weite122@foxmail.com>
2020-07-13 09:12:15 +0800

    [A]引入iconfont

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -14,6 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options)
     musiclist = wx.getStorageSync('musiclist')
     nowPlayingIndex = options.index
     this._loadMusicDetail()
