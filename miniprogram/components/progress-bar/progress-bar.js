a3b7871ab4d1b8fc85a02f78f3eb9f357c171ef5
weite122 <weite122@foxmail.com>
2020-07-14 01:55:27 +0800

    [A]添加并完成progress-bar组件基本样式

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,28 @@
+// components/progress-bar/progress-bar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showTime: {
+      currentTime: '00:00',
+      totalTime: '00:00'
+    },
+    movableDis: 0,
+    percent: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
