2b9c788bf9d433242d332856d76dfbfed97a850b
weite122 <weite122@foxmail.com>
2020-07-15 16:40:50 +0800

    feat:完成歌词解析，歌词页面基本样式

--- miniprogram/components/lyric/lyric.js
+++ miniprogram/components/lyric/lyric.js
@@ -13,7 +13,7 @@ Component({
 
   observers: {
     lyric(lrc) {
-      console.log(lrc)
+      this._parseLyric(lrc)
     }
   },
   /**
@@ -27,6 +27,24 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    _parseLyric(sLyric){
+      let line = sLyric.split('\n')
+      let _lrcList = []
+      line.forEach((elem) => {
+        let time = elem.match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g)
+        if (time != null) {
+          let lrc = elem.split(time)[1]
+          let timeReg = time[0].match(/(\d{2,}):(\d{2})(?:\.(\d{2,3}))?/)
+          let time2Seconds = parseInt(timeReg[1]) * 60 + parseInt(timeReg[2]) + parseInt(timeReg[3]) / 1000
+          _lrcList.push({
+            lrc,
+            time: time2Seconds,
+          })
+        }
+      })
+      this.setData({
+        lrcList: _lrcList
+      })
+    }
   }
 })
