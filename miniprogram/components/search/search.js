3d85d8e2dfc1127154a787d3a3b20b104829f779
weite122 <weite122@foxmail.com>
2020-07-20 19:48:06 +0800

    feat: 添加blog，search组件

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -1,11 +1,18 @@
-// pages/blog/blog.js
+// components/search/search.js
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    placeholder:{
+      type: String,
+      value: '请输入关键字'
+    }
   },
+  externalClasses: [
+    'iconfont',
+    'icon-sousuo'
+  ],
 
   /**
    * 组件的初始数据
@@ -14,9 +21,6 @@ Component({
 
   },
 
-  onPublish(){
-
-  },
   /**
    * 组件的方法列表
    */
