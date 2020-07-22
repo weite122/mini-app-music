630012cf2eb42debd6508eeffc32621e1aca87b0
weite122 <weite122@foxmail.com>
2020-07-22 20:42:54 +0800

    feat: 封装formatTime函数，格式化云数据库的时间

--- miniprogram/components/blog-card/blog-card.js
+++ miniprogram/components/blog-card/blog-card.js
@@ -1,4 +1,5 @@
 // components/blog-card/blog-card.js
+import formatTime from '../../utils/formatTime.js'
 Component({
   /**
    * 组件的属性列表
@@ -7,11 +8,20 @@ Component({
     blog: Object
   },
 
+  observers: {
+    ['blog.createTime'](val){
+      if(val){
+        this.setData({
+          _createTime: formatTime(new Date(val))
+        })
+      }
+    }
+  },
   /**
    * 组件的初始数据
    */
   data: {
-
+    _createTime: ''
   },
 
   /**
